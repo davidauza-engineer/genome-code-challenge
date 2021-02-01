809967f4643e6ab9adb8cedecc0f3956d44c49b5
davidauza.engineer <davidauza01@gmail.com>
2021-01-31 19:15:00 -0500

    Fix rubocop issues

--- spec/views/cities/home.html.erb_spec.rb
+++ spec/views/cities/home.html.erb_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'cities/home.html.erb', type: :view do
   before { render }
 
   it 'contains Weather App title' do
-    expect(rendered).to match /Weather App/
+    expect(rendered).to match(/Weather App/)
   end
 
   it 'contains the Search button' do
-    expect(rendered).to match /Search/
+    expect(rendered).to match(/Search/)
   end
 
   it 'contains Search for your city label' do
-    expect(rendered).to match /Search for your city/
+    expect(rendered).to match(/Search for your city/)
   end
 end
